6c870919698e0eed24d948b87b57e848b7a34b50
Sriram Sankaranarayanan <sriram.sankaranarayanan@polymtl.ca>
2020-03-03 18:07:38 -0500

    pointers

--- 05_pointers.cpp
+++ 05_pointers.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 
-int main() {}
+void increment(int *x)
+{
+	(*x)++;
+}
+
+int main() {
+	int regInt1 {42};
+	int *regInt1Point;
+	regInt1Point = &regInt1;
+	// Printing them
+	std::cout << "regInt1: "<<regInt1<<'\n';
+	std::cout << "regInt1Point: "<<regInt1Point<<'\n';
+	std::cout << "*regInt1Point: "<<*regInt1Point<<'\n'; 
+
+	*regInt1Point = 27;
+	std::cout << "regInt1: "<<regInt1<<'\n';
+	std::cout << "regInt1Point: "<<regInt1Point<<'\n';
+	std::cout << "*regInt1Point: "<<*regInt1Point<<'\n'; 
+
+	increment(regInt1Point);
+	std::cout << "regInt1: "<<regInt1<<'\n';
+	std::cout << "regInt1Point: "<<regInt1Point<<'\n';
+	std::cout << "*regInt1Point: "<<*regInt1Point<<'\n'; 
+
+	int array[5] = {1,2,3,4,5};
+	int* arrPoint = &array[0];
+	std::cout << arrPoint << " " << arrPoint + 1 << '\n';
+	std::cout << *arrPoint << " " << *(arrPoint + 1) << '\n';
+
+	// Don't use them unless absolutely necessary in modern code!
+}
+
+
+
+
